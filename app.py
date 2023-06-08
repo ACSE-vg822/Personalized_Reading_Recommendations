7d9a57b798536adb3fb7cf0929c5dc3476891b42
acse-23a8b65a <vidusheegeetam@gmail.com>
2023-06-08 20:56:35 +0100

    more sustainable

--- app.py
+++ app.py
@@ -1,34 +1,13 @@
+from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 
-# Load the cosine similarity matrix from the .npy file
-cosine_sim = np.load('File_dump/cosine_sim.npy')
+from similarity_matrix import compute_similarity_matrix
+from recommendation import get_recommendations
+
 indices = pd.read_pickle('File_dump/indices.pkl')
 df = pd.read_pickle('File_dump/df.pkl')
-
-def get_recommendations(title, cosine_sim=cosine_sim):
-    # Get the index of the book that matches the title
-    idx = indices[title]
-
-    # Get the pairwise similarity scores of all books with that book
-    sim_scores = list(enumerate(cosine_sim[idx].flatten()))
-
-    # Sort the books based on the similarity scores
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    # Get the scores of the 10 most similar books
-    sim_scores = sim_scores[1:11]
-
-    # Get the book indices
-    book_indices = [i[0] for i in sim_scores]
-    
-    # Return the top 10 most similar books
-    return df.iloc[book_indices]
-
-#print(get_recommendations('A Short History of Nearly Everything'))
-
-from flask import Flask, render_template, request
-import pandas as pd
+cosine_sim = compute_similarity_matrix(df)
 
 app = Flask(__name__)
 
@@ -36,7 +15,7 @@ app = Flask(__name__)
 def process_input():
     if request.method == 'POST':
         user_input = request.form['input']
-        result = get_recommendations(user_input)
+        result = get_recommendations(user_input, cosine_sim, indices, df)
         result_table = result.to_html()
         return render_template('result.html', table=result_table)
     return render_template('index.html')
